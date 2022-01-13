99615b62041fc38adc6ba1e45d5f116b2ea9a1de
minsun <meeeeensun@gmail.com>
2022-01-13 20:17:53 +0900

    [PGS] 징검다리 / level4 / 40분 / Algo2022#18

---
+++ k12ms26/search/징검다리.js
@@ -0,0 +1,33 @@
+//programmers: 징검다리
+//https://gwang920.github.io/algorithm/progreammers-2-43236/
+function solution(distance, rocks, n) {
+    let answer = 0;
+    rocks=[0, ...rocks.sort((a, b)=>a-b), distance];
+    
+    const BinarySearch = () =>{
+        let start = rocks[0], end = rocks[rocks.length-1];
+        
+        while(start <= end){
+            let mid = Math.floor((start+end)/2);
+            let count=0, now=0;
+            for(let i=1;i<rocks.length;i++){ //이거 왜해?
+                if(rocks[i]-now < mid){ //기준값보다 현재 값이 작으면 count + 1
+                    count++;   
+                } else {
+                    now = rocks[i]; //크면 기준값으로
+                }
+            }
+            
+            if(count>n){ //count가 제거할 바위의 개수보다 크면
+                end=mid-1; //줄여라
+            }else{
+                start=mid+1; //아니면 늘여라
+            }
+        }
+
+        answer = end;
+    }
+    
+    BinarySearch();
+    return answer;
+}
