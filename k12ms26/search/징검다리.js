a8f47c280def81137d800754c31bd784d5c7378a
minsun <meeeeensun@gmail.com>
2022-01-16 21:38:52 +0900

    [PGS] 징검다리 / level4 / 40분 / Algo2022#18
    수정

--- k12ms26/search/징검다리.js
+++ k12ms26/search/징검다리.js
@@ -1,6 +1,5 @@
 //programmers: 징검다리
 //https://gwang920.github.io/algorithm/progreammers-2-43236/
-//바위 개수와 제거할 바위의 개수가 같은 경우는 안 고려하는건가?
 function solution(distance, rocks, n) {
     let answer = 0;
     rocks=[0, ...rocks.sort((a, b)=>a-b), distance];
@@ -10,24 +9,23 @@ function solution(distance, rocks, n) {
         
         while(start <= end){
             let mid = Math.floor((start+end)/2);
-            let count=0, now=0;
+            let count = 0, now = rocks[0];
             //제거할 바위를 고르는 법
-            for(let i=1;i<rocks.length;i++){ //이거 왜해?
-                if(rocks[i]-now < mid){ //기준값보다 현재 값이 작으면 count + 1
+            for(let i=1;i<rocks.length;i++){
+                if(rocks[i] - now < mid){ //징검다리 간의 간격이 기준값보다 작으면
                     count++;   
                 } else {
                     now = rocks[i]; //크면 기준값으로
                 }
             }
             
-            if(count>n){ //count가 제거할 바위의 개수보다 크면
-                end=mid-1; //줄여라
-            }else{
-                start=mid+1; //아니면 늘여라
+            if(count > n){ //count가 제거할 바위의 개수보다 크면
+                end = mid - 1; //줄여라 (더 제거할 바위의 개수가 작아져야하니까)
+            } else {
+                start = mid + 1; //아니면 늘여라
             }
         }
-
-        answer = end;
+        answer = end; //최소값의 최댓값
     }
     
     BinarySearch();
