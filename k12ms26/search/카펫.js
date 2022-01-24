e920d4be86708b44fcf1328afc0e92da3a98eea0
minsun <meeeeensun@gmail.com>
2022-01-25 00:07:28 +0900

    [PGS] 카펫 / level2 / 30분 / Algo2022#27

---
+++ k12ms26/search/카펫.js
@@ -0,0 +1,21 @@
+//programmers: 카펫
+function solution(brown, yellow) {
+    let row = 0, col = 0;
+    
+    let isDone = false;
+    for(let i=1;i<=yellow;i++) {
+        for(let j=1;j<=yellow;j++) {
+            if(i * j === yellow && i >= j && (brown - 4) % (i + j) === 0) { //4,6,7,9 에러 -> [5, 4] TC 돌려보기
+                row += i;
+                col += j;
+                isDone = true;
+                break;
+            }
+        }
+        if(isDone) break;
+    }
+    
+    if(brown - 4 === (row + col) * 2) return [row+2, col+2];
+    
+    return [row+2, col+2];
+}
