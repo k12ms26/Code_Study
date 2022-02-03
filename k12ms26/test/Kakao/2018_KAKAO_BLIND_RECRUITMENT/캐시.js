ce2f8e65f4f225a07445f4554261c3bb0cd5b44e
minsun <meeeeensun@gmail.com>
2022-02-03 19:50:19 +0900

    [PGS] 캐시 / level2 / 40분 / Algo2022#34

---
+++ k12ms26/test/Kakao/2018_KAKAO_BLIND_RECRUITMENT/캐시.js
@@ -0,0 +1,25 @@
+//programmers: 캐시
+//LRU 알고리즘: 가장 오래된 것 교체
+function solution(cacheSize, cities) {
+    let cache = [], idx = 0;
+    let total = 0;
+    
+    if(cacheSize === 0) return 5 * cities.length;
+    
+    for(let c of cities) {
+        let city = c.toUpperCase();
+        let nowIdx = cache.findIndex(e => e === city);
+        if(nowIdx > -1) { //65점 -> 100점: 중복되는게 들어왔을 경우 해당 것을 지워주고 새롭게 push
+            cache.splice(nowIdx, 1);
+        }
+        if(cache.length === cacheSize) {
+            cache.shift();
+        }
+        cache.push(city);
+
+        if(nowIdx > -1) total += 1;
+        else total += 5;
+
+    }
+    return total;
+}
