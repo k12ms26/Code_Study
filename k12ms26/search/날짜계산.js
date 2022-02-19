ac4ed267a6f80cd2db1ddd1aacdb2fc4766b62d7
minsun <meeeeensun@gmail.com>
2022-02-19 18:44:18 +0900

    [BOJ] 1476 날짜 계산 / silver5 / 20분 / Algo2022#42
    js파일 수정

--- k12ms26/search/날짜계산.js
+++ k12ms26/search/날짜계산.js
@@ -1,5 +1,4 @@
 //BOJ 1476 날짜 계산
-//메모리 초과
 const input = [];
 require("readline")
   .createInterface(process.stdin, process.stdout)
@@ -9,13 +8,15 @@ require("readline")
   .on("close", () => {
     const [E, S, M] = input[0].split(' ').map(Number);
     //1 ≤ E ≤ 15, 1 ≤ S ≤ 28, 1 ≤ M ≤ 19
-    if(E === 1 && S === 1 && M === 1) return 1;
-    let year = 1;
-    while(true) {
-        year++;
-        if((year-E) % 15 === 0 && (year-S) % 28 === 0 && (year-M) % 19 === 0) break;
+    if(E === 1 && S === 1 && M === 1) console.log(1);
+    else {
+        let year = 1;
+        while(true) {
+            year++;
+            if((year-E) % 15 === 0 && (year-S) % 28 === 0 && (year-M) % 19 === 0) break;
+        }
+        console.log(year);
     }
 
-    console.log(year);
     process.exit();
 });
