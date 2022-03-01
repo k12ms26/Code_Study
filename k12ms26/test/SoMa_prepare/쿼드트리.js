213a468120fd71e24fe6d38dc358527aa7c53c9f
minsun <meeeeensun@gmail.com>
2022-03-01 23:30:23 +0900

    [BOJ] 1992 쿼드트리 / silver1 / 1시간 / Algo2022#50

--- k12ms26/test/SoMa_prepare/쿼드트리.js
+++ k12ms26/test/SoMa_prepare/쿼드트리.js
@@ -1,4 +1,5 @@
 //BOJ 1992 쿼드트리
+//https://tesseractjh.tistory.com/94
 // let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 const input = [];
 require("readline")
@@ -12,5 +13,31 @@ require("readline")
 });
 
 const solution = () => {
+    const n = +input[0];
+    const arr = input.slice(1).map(v => v.split("").map(Number));
 
+    const quadTree = [];
+    const recursion = (n, x, y) => {
+        if(x < n && y < n) {}
+        let total = 0;
+        for(let i=0;i<n;i++) {
+            for(let j=0;j<n;j++) {
+                total += arr[y+j][x+i];
+            }
+        }
+
+        if (total === 0) quadTree.push("0");
+        else if (total === n*n) quadTree.push("1");
+        else {
+            n /= 2;
+            quadTree.push("(");
+            recursion(n, x, y);
+            recursion(n, x+n, y);
+            recursion(n, x, y+n);
+            recursion(n, x+n, y+n);
+            quadTree.push(")");
+        }
+    }
+    recursion(n, 0, 0);
+    return quadTree.join("");
 };
